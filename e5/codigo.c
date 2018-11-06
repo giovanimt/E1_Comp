3e754e273352c205f01a4a184dce8143b0203e50
giovanimt <giovanimt@outlook.com>
2018-11-06 19:43:54 -0200

    adicionadas funcoes que criam rotulos e registradores

---
+++ e5/codigo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "codigo.h"
+
+int num_rotulos = 0;
+int num_regs = 0;
+
+
+//Função que gera o nome de um rótulo
+char* gera_rotulo(){
+	char *nome_rotulo = (char*)malloc(20);
+	sprintf(nome_rotulo, "L%i", num_rotulos);
+	num_rotulos++;		
+	return nome_rotulo;
+}
+
+
+
+//Função que gera o nome de um registrador
+char* gera_registrador(){
+	static int num_regs = 1;
+	char *nome_reg = (char*)malloc(20);
+	sprintf(nome_reg, "r%i", num_regs);
+	num_regs++;		
+	return nome_reg;
+}
