6837735734d05f9bea3d3d45b09c80aade13f946
vfcastro <vfcastro@inf.ufrgs.br>
2018-11-11 14:55:43 -0200

    varios erros de compilacao; vide comentarios Vinicius: no código

--- e5/codigo.c
+++ e5/codigo.c
@@ -39,38 +39,41 @@ void gera_codigo_vg(NodoArvore *n){
 void gera_codigo_vl(NodoArvore *n){
     printf("addI rsp, 4 => rsp\n");
     //Se existe atribuicao juntamente com a declaracao:
-    if(n->filho[5]){
-        switch(n->filho[5]->nodo.valor_lexico.type) {
+    if(n->filhos[5]){
+        switch(n->filhos[5]->nodo.valor_lexico.type) {
 
+        // Vinicius: Comentando, pois não compila (não pode ter declaração dentro dos case)
 		//se for inteiro
-		case(INTEIRO):
+		/*case(INTEIRO):
 			int des_vl = 0; //TODO:descobrir deslocamento da variavel declarada em relacao a rfp
 			char* reg_temp = gera_registrador();
 			printf("loadI %d => %s\n",n->filho[5]->nodo.valor_lexico.val.int_val, reg_temp);
-			printf("storeAI %s => rfp, %d\n", reg_temp, des_vl)
-			break;
+			printf("storeAI %s => rfp, %d\n", reg_temp, des_vl);
+			break; */
 
 		//se for TK_IDENTIFICADOR
 		default:
-			int des_ident = 0; //TODO:descobrir deslocamento da variavel de TK_IDENTIFICADOR em relacao a rfp
-			int des_vl = 0; //TODO:descobrir deslocamento da variavel declarada em relacao a rfp
+		    // Vinicius: Comentando, pois não compila e não entendi (não pode ter declaração dentro dos case)
+			//int des_ident = 0; //TODO:descobrir deslocamento da variavel de TK_IDENTIFICADOR em relacao a rfp
+			//int des_vl = 0; //TODO:descobrir deslocamento da variavel declarada em relacao a rfp
 
-			if(true){//TODO: fazer if para descobrir se TK_IDENTIFICADOR eh vl ou vg, se for vg:
+            // Vinicius: Comentando, pois não compila e não entendi o if(true)
+			/*if(true){//TODO: fazer if para descobrir se TK_IDENTIFICADOR eh vl ou vg, se for vg:
 				char* reg_temp2 = gera_registrador();
-				print("loadAI rbss,%d => %s\n", des_ident, reg_temp2);
+				printf("loadAI rbss,%d => %s\n", des_ident, reg_temp2);
 				printf("storeAI %s => rfp, %d\n", reg_temp2, des_vl);
 			}else{//se TK_IDENT for vl:
 				char* reg_temp2 = gera_registrador();
-				print("loadAI rfp,%d => %s\n", des_ident, reg_temp2);
-				printf("storeAI %s => rfp, %d\n", reg_temp2, des_vl);
-			break;
+				printf("loadAI rfp,%d => %s\n", des_ident, reg_temp2);
+				printf("storeAI %s => rfp, %d\n", reg_temp2, des_vl); */
+			break; 
         }
     }
 }
 
 
 //Gera codigo de atribuicao TK_IDENTIFICADOR '=' expressao
-gera_codigo_atr(NodoArvore *n){
+void gera_codigo_atr(NodoArvore *n){
 	printf("nop\n");
 	/*TODO: para os casos:
 	exp_literal
@@ -92,15 +95,15 @@ gera_codigo_atr(NodoArvore *n){
 	*/
 }
 
-gera_codigo_if(NodoArvore *n){
+void gera_codigo_if(NodoArvore *n){
 	printf("nop\n");
 }
 
-gera_codigo_while(NodoArvore *n){
+void gera_codigo_while(NodoArvore *n){
 	printf("nop\n");
 }
 
-gera_codigo_do(NodoArvore *n){
+void gera_codigo_do(NodoArvore *n){
 	printf("nop\n");
 }
 
